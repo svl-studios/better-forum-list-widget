1659d09bf4a1eeb56101bd78c18a7f3bf0a9d660
Kevin Provance <kevin.provance@gmail.com>
2021-04-04 16:03:37 -0400

    PHPCS

--- better-forum-list-widget.php
+++ better-forum-list-widget.php
@@ -10,69 +10,84 @@ Author URI: http://www.bavotasan.com/
 /**
  * Register the widget
  */
-add_action( 'widgets_init', function() {
-     register_widget( 'BBP_Forums_Topic_Count_Widget' );
-} );
+add_action(
+	function () {
+		register_widget( 'BBP_Forums_Topic_Count_Widget' );
+	}
+);
 
+/**
+ * Class BBP_Forums_Topic_Count_Widget
+ */
 class BBP_Forums_Topic_Count_Widget extends WP_Widget {
 
 	/**
 	 * Setting up the basic widget requirements
 	 */
 	public function __construct() {
-		$widget_ops = array( 'classname' => 'bbp_forums_topic_count_widget_options', 'description' => __( 'A list of forums with their topic count' ) );
+		$widget_ops = array(
+			'classname'   => 'bbp_forums_topic_count_widget_options',
+			'description' => __( 'A list of forums with their topic count' ),
+		);
+
 		parent::__construct( false, __( '(bbPress) Forums List with Topic Count' ), $widget_ops );
 	}
 
 	/**
 	 * Everything that we need to display the widget on the front end
 	 *
-	 * @param	arr $args
-	 * @param	arr $instance
+	 * @param arr $args Arguments.
+	 * @param arr $instance Instance.
 	 *
-	 * @uses	bbp_get_statistics()
-	 * @uses	wp_list_pages()
-	 * @uses	bbp_get_forum_post_type()
-	 * @uses	bbp_get_forum_post_type()
+	 * @uses    bbp_get_statistics()
+	 * @uses    wp_list_pages()
+	 * @uses    bbp_get_forum_post_type()
+	 * @uses    bbp_get_forum_post_type()
 	 */
 	public function widget( $args, $instance ) {
+
+		// phpcs:ignore WordPress.PHP.DontExtract
 		extract( $args );
+
 		$title = apply_filters( 'bbp_forum_widget_title', $instance['title'] );
 
-		echo $before_widget;
+		echo wp_kses_post( $before_widget );
 
-		if ( ! empty( $title ) )
-			echo $before_title . $title . $after_title;
+		if ( ! empty( $title ) ) {
+			echo wp_kses_post( $before_title . $title . $after_title );
+		}
 		?>
 		<ul>
 			<?php
-			wp_list_pages( array(
-				'title_li' => '',
-				'post_type' => bbp_get_forum_post_type(),
-				'sort_column' => 'menu_order',
-				'walker' => new Forum_List_Walker,
-				'no_found_rows' => true,
-			) );
+			wp_list_pages(
+				array(
+					'title_li'      => '',
+					'post_type'     => bbp_get_forum_post_type(),
+					'sort_column'   => 'menu_order',
+					'walker'        => new Forum_List_Walker(),
+					'no_found_rows' => true,
+				)
+			);
 			?>
 		</ul>
 		<?php
-		echo $after_widget;
+		echo wp_kses_post( $after_widget );
 	}
 
 	/**
 	 * The widget admin options
 	 *
-	 * @param	arr $instance
+	 * @param arr $instance Instance.
 	 *
-	 * @uses	get_field_id()
-	 * @uses	get_field_name()
+	 * @uses    get_field_id()
+	 * @uses    get_field_name()
 	 */
 	public function form( $instance ) {
 		$title = empty( $instance['title'] ) ? '' : esc_attr( $instance['title'] );
 		?>
 		<p>
-			<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?>
-				<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo $title; ?>" />
+			<label for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"><?php esc_html_e( 'Title:' ); ?>
+				<input class="widefat" id="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>" name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>"/>
 			</label>
 		</p>
 		<?php
@@ -81,79 +96,107 @@ class BBP_Forums_Topic_Count_Widget extends WP_Widget {
 	/**
 	 * Validate the widget admin options
 	 *
-	 * @param	arr $new_instance
-	 * @param	arr $old_instance
+	 * @param arr $new_instance Old instance.
+	 * @param arr $old_instance New instance.
+	 *
+	 * @retun array
 	 */
-	public function update( $new_instance, $old_instance ) {
-		$instance = $old_instance;
-		$instance['title'] = strip_tags( $new_instance['title'] );
+	public function update( $new_instance, $old_instance ): array {
+		$instance          = $old_instance;
+		$instance['title'] = wp_strip_all_tags( $new_instance['title'] );
 
 		return $instance;
 	}
 }
 
-class Forum_List_Walker extends Walker {
-	var $tree_type = 'page';
-	var $db_fields = array (
+/**
+ * Class Forum_List_Walker
+ */
+class Forum_List_Walker extends Walker { // phpcs:ignore
+
+	/**
+	 * Tree type.
+	 *
+	 * @var string
+	 */
+	public $tree_type = 'page';
+
+	/**
+	 * Database fields.
+	 *
+	 * @var string[]
+	 */
+	public $db_fields = array(
 		'parent' => 'post_parent',
-		'id' => 'ID'
+		'id'     => 'ID',
 	);
 
 	/**
 	 * Used to output the opening HTML tag at the beginning of our list item
 	 *
-	 * @param	str $output
-	 * @param	int $depth
-	 * @param	arr $args
+	 * @param str $output Output.
+	 * @param int $depth Depth.
+	 * @param arr $args Args.
 	 */
-	function start_lvl( &$output, $depth = 0, $args = array() ) {
-		$indent = str_repeat( "\t", $depth );
+	public function start_lvl( &$output, $depth = 0, $args = array() ) {
+		$indent  = str_repeat( '\t', $depth );
 		$output .= "\n$indent<ul class='children'>\n";
 	}
 
 	/**
 	 * Used to output the closing HTML tag at the end of our list item
 	 *
-	 * @param	str $output
-	 * @param	int $depth
-	 * @param	arr $args
+	 * @param str $output Output.
+	 * @param int $depth Depth.
+	 * @param arr $args Args.
 	 */
-	function end_lvl( &$output, $depth = 0, $args = array() ) {
-		$indent = str_repeat( "\t", $depth );
+	public function end_lvl( &$output, $depth = 0, $args = array() ) {
+		$indent  = str_repeat( '\t', $depth );
 		$output .= "$indent</ul>\n";
 	}
 
 	/**
 	 * Used to output the HTML at the beginning of our list
 	 *
-	 * @param	str $output
-	 * @param	int $depth
-	 * @param	arr $args
-	 * @param	arr $page
+	 * @param str $output            Output.
+	 * @param arr $object            Page.
+	 * @param int $depth             Depth.
+	 * @param arr $args              Args.
+	 * @param int $current_object_id ID.
 	 */
-	function start_el( &$output, $page, $depth = 0, $args = array(), $current_object_id = 0 ) {
+	public function start_el( &$output, $object, $depth = 0, $args = array(), $current_object_id = 0 ) {
 		$indent = ( $depth ) ? str_repeat( "\t", $depth ) : '';
 
+		// phpcs:ignore WordPress.PHP.DontExtract
 		extract( $args, EXTR_SKIP );
-		$css_class = array();
-		$has_children = forum_list_widget_has_children( $page->ID );
-		if ( ! bbp_is_single_user() && $current_page = get_the_ID() ) {
+
+		$css_class    = array();
+		$has_children = forum_list_widget_has_children( $object->ID );
+
+		$current_page = get_the_ID();
+		if ( ! bbp_is_single_user() && $current_page ) {
 			$_current_page = get_post( $current_page );
-			if ( in_array( $page->ID, $_current_page->ancestors ) )
+
+			if ( in_array( $object->ID, $_current_page->ancestors, true ) ) {
 				$css_class[] = 'current_forum_ancestor';
-			if ( $has_children )
+			}
+
+			if ( $has_children ) {
 				$css_class[] = 'forum_category';
-			if ( $page->ID == $current_page )
+			}
+
+			if ( $object->ID === $current_page ) {
 				$css_class[] = 'current_forum_item';
-			elseif ( $_current_page && $page->ID == $_current_page->post_parent )
+			} elseif ( $_current_page && $object->ID === $_current_page->post_parent ) {
 				$css_class[] = 'current_forum_parent';
-		} elseif ( $page->ID == get_option( 'page_for_posts' ) ) {
+			}
+		} elseif ( get_option( 'page_for_posts' ) === $object->ID ) {
 			$css_class[] = 'current_forum_parent';
 		}
 
-		$css_class = implode( ' ', $css_class );
-		$topic_count = ( $has_children ) ? '' : '<span class="topic-count">' . bbp_get_forum_topic_count( $page->ID ) . '</span>';
-		$forum_item = ( $has_children ) ? '<span class="forum_category_title">' . apply_filters( 'the_title', $page->post_title, $page->ID ) . '</span>' . $topic_count : '<a href="' . get_permalink( $page->ID ) . '">' . apply_filters( 'the_title', $page->post_title, $page->ID ) . $topic_count . '</a>';
+		$css_class   = implode( ' ', $css_class );
+		$topic_count = ( $has_children ) ? '' : '<span class="topic-count">' . intval( bbp_get_forum_topic_count( $object->ID ) ) . '</span>';
+		$forum_item  = ( $has_children ) ? '<span class="forum_category_title">' . apply_filters( 'the_title', $object->post_title, $object->ID ) . '</span>' . $topic_count : '<a href="' . get_permalink( $object->ID ) . '">' . apply_filters( 'the_title', $object->post_title, $object->ID ) . $topic_count . '</a>';
 
 		$output .= $indent . '<li class="' . $css_class . '">' . $forum_item;
 	}
@@ -161,43 +204,51 @@ class Forum_List_Walker extends Walker {
 	/**
 	 * Used to output the HTML at the end of our list
 	 *
-	 * @param	str $output
-	 * @param	int $depth
-	 * @param	arr $args
-	 * @param	arr $page
+	 * @param str $output Output.
+	 * @param arr $object Page.
+	 * @param int $depth  Depth.
+	 * @param arr $args   Args.
 	 */
-	function end_el( &$output, $page, $depth = 0, $args = array() ) {
+	public function end_el( &$output, $object, $depth = 0, $args = array() ) {
 		$output .= "</li>\n";
 	}
-
 }
 
 /**
  * Conditional check to see if page has sub-pages
  *
- * @param	str $page_id
+ * @param mixed $page_id Page ID.
  *
- * @uses	bbp_get_forum_post_type()
+ * @uses    bbp_get_forum_post_type()
  */
 function forum_list_widget_has_children( $page_id ) {
-	$children = get_pages( array(
-		'child_of' => $page_id,
-		'post_type' => bbp_get_forum_post_type(),
-	) );
-	if ( $children )
+	$children = get_pages(
+		array(
+			'child_of'  => $page_id,
+			'post_type' => bbp_get_forum_post_type(),
+		)
+	);
+
+	if ( $children ) {
 		return true;
-	else
+	} else {
 		return false;
+	}
 }
 
 add_action( 'wp_enqueue_scripts', 'forum_list_widget_styles' );
+
 /**
  * Enqueue the stylesheet
- *
  * This function is attached to the 'wp_enqueue_scripts' action hook
  *
- * @uses	wp_enqueue_style()
+ * @uses    wp_enqueue_style()
  */
 function forum_list_widget_styles() {
-	wp_enqueue_style( 'forum_list_widget_styles', plugins_url( '/css/style.css', __FILE__ ) );
+	wp_enqueue_style(
+		'forum_list_widget_styles',
+		plugins_url( '/css/style.css', __FILE__ ),
+		array(),
+		'1.0.0'
+	);
 }
